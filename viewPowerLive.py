e46ac02d4322e1629cb1003d6e32929596be3554
OB-UNISA <68842946+OB-UNISA@users.noreply.github.com>
2022-11-13 09:55:00 +0100

    added ip as an optional arg, refactored some code to improve performance, renamed functions. The script will not turn on the wifi and power LED.

--- viewPowerLive.py
+++ viewPowerLive.py
@@ -2,18 +2,17 @@ import os
 import requests
 import matplotlib.pyplot as plt
 import argparse
+import re
 
 from matplotlib.animation import FuncAnimation
 from dotenv import load_dotenv
 from datetime import datetime
 
-load_dotenv()
-
-SHELLYPLUG_IP = os.getenv('SHELLYPLUG_IP')
-
 
 class PowerLive:
-    def __init__(self, buffer_length, vertical=True, verbose=False):
+    def __init__(self, ip, buffer_length, vertical=True, verbose=False):
+        self.ip = ip
+        self._ip_data = f'http://{self.ip}/meter/0'
         self.buffer_length = buffer_length
         self.verbose = verbose
 
@@ -27,7 +26,7 @@ class PowerLive:
         else:
             self.fig, (self.ax1, self.ax2) = plt.subplots(1, 2)
 
-        self.fig.suptitle(f'Shelly Plug Power Live - {datetime.utcnow()}')
+        self.fig.suptitle(f'Shelly Plug Power Live - {datetime.utcnow()} (UTC)')
         self.ax1.set_xlabel('Time (s)')
         self.ax1.set_ylabel('Power (W)')
         self.ax1.grid()
@@ -38,25 +37,32 @@ class PowerLive:
         self.ln2, = self.ax2.plot([], [], 'g-')
         self.ani = FuncAnimation(self.fig, self.update, interval=1000)
 
-        requests.get(f'http://{SHELLYPLUG_IP}/relay/0?turn=on')
+        requests.get(f'http://{self.ip}/settings?led_status_disable=false')
+        requests.get(f'http://{self.ip}/settings?led_power_disable=false')
+        requests.get(f'http://{self.ip}/relay/0?turn=on')
 
         plt.show()
 
     def update(self, frame):
-        data = requests.get(f'http://{SHELLYPLUG_IP}/meter/0').json()
+        data = requests.get(self._ip_data).json()
         if self.verbose:
             print(data)
-        self.update_append(data)
-        self.update_no_append(data)
+        self.update_full_graph(data)
+        self.update_buffer_graph(data)
 
         return self.ln1, self.ln2
 
-    def update_append(self, data):
+    def update_full_graph(self, data):
         self.x1.append(self.x1[-1] + 1)
         self.y1.append(data['power'])
         self.update_set_data(self.ax1, self.ln1, self.x1, self.y1)
 
-    def update_no_append(self, data):
+    # A better solution would be to use the original data in x1 and y1 and pass to the buffer only the indexes.
+    # But it seems it can not be done in Python. If you use Slice operator, it will create a copy of the list arr[a:b]
+    # from index a to b which means at every interval a list of length b-a will be created. The current solution only
+    # requires 2 operations on the list and additional space for the list, compared to the Slice which would requires
+    # b-a operations.
+    def update_buffer_graph(self, data):
         self.x2.pop(0)
         self.x2.append(self.x2[-1] + 1)
         self.y2.pop(0)
@@ -70,7 +76,7 @@ class PowerLive:
         ax.autoscale_view()
 
     def __del__(self):
-        requests.get(f'http://{SHELLYPLUG_IP}/relay/0?turn=off')
+        requests.get(f'http://{self.ip}/relay/0?turn=off')
 
 
 if __name__ == '__main__':
@@ -82,11 +88,25 @@ if __name__ == '__main__':
         return ivalue
 
 
+    def ip_type(value):
+        if not re.match(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', value):
+            raise argparse.ArgumentTypeError(f'{value} is an invalid IP address')
+
+        return value
+
+
+    load_dotenv()
+
     parser = argparse.ArgumentParser()
+    parser.add_argument('-ip', '--ip', type=ip_type, default=os.getenv('SHELLYPLUG_IP'), help='Shelly Plug IP')
     parser.add_argument('-hr', '--horizontal', action='store_true', help='horizontal layout, default is vertical')
     parser.add_argument('-b', '--buffer_length', type=buffer_length_type, default=30,
                         help='buffer length in seconds, must be a positive int value >= 2. Default is 30')
     parser.add_argument('-v', '--verbose', action='store_true',
                         help='verbose mode, print data to console. Default is False')
     args = parser.parse_args()
-    PowerLive(args.buffer_length, not args.horizontal)
+
+    if not args.ip:
+        exit('Shelly Plug IP is not set, please set it in .env file or pass it as argument')
+
+    PowerLive(args.ip, args.buffer_length, vertical=not args.horizontal)
