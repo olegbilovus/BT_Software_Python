f0a050c4b006551e338fb4d16b20f6e96d0689b1
Oleg Bilovus <68842946+OB-UNISA@users.noreply.github.com>
2022-12-01 15:15:30 +0100

    added threads to manage the data. Removed the buffer graph because it was causing low performance. Added an optional argument to limit the number of captures. (#1)
    Signed-off-by: Oleg Bilovus <68842946+OB-UNISA@users.noreply.github.com>
    
    Signed-off-by: Oleg Bilovus <68842946+OB-UNISA@users.noreply.github.com>

--- powerLive.py
+++ powerLive.py
@@ -1,6 +1,9 @@
 import argparse
+import os
+import queue
 import re
 import sqlite3
+import threading
 import time
 from abc import ABC, abstractmethod
 from datetime import datetime
@@ -77,14 +80,15 @@ class NetioPowerCableRest101(Plug):
 
 
 class PowerLive:
-    def __init__(self, plug: Plug, buffer_length, vertical=True, db_name=None, db_reset=False, no_graph=False,
+    def __init__(self, plug: Plug, db_name=None, db_reset=False, no_graph=False, n_threads=3, captures_limit=None,
                  verbose=False):
         self.plug = plug
-        self.buffer_length = buffer_length
         self.verbose = verbose
+        self.captures_limit = captures_limit
+        self.captures = 0
 
         self.db_name = db_name
-        self.conn = sqlite3.connect(db_name)
+        self.conn = sqlite3.connect(db_name, check_same_thread=False)
         self.cur = self.conn.cursor()
         try:
             self.cur.execute(
@@ -93,107 +97,90 @@ class PowerLive:
             print('Created table')
         except sqlite3.OperationalError:
             pass
+        self._sql_query = 'INSERT INTO plug_load VALUES (?, ?, ?)'
 
         if db_reset:
             self.cur.execute('DELETE FROM plug_load')
             self.conn.commit()
             print('Deleted all rows from table')
-        self.cur.execute('INSERT INTO plug_load VALUES (?, ?, ?)', (datetime.utcnow(), 0, 0))
+        self.cur.execute(self._sql_query, (datetime.utcnow(), 0, 0))
         print(f'Data will be saved to {db_name}')
 
         if not self.plug.turn_on():
             exit('Failed to turn on plug')
 
+        start = queue.Queue()
+        self._lock = threading.Lock()
         if not no_graph:
+            init_data = self.get_data()
             self.x1 = [1]
-            self.y1 = [self.plug.get_load()]
-            self.x2 = [1]
-            self.y2 = [self.y1[0]]
+            self.y1 = [init_data['power']]
+            self.send_to_sql(init_data)
 
-            if vertical:
-                self.fig, (self.ax1, self.ax2) = plt.subplots(2)
-            else:
-                self.fig, (self.ax1, self.ax2) = plt.subplots(1, 2)
-
+            self.fig, self.ax1 = plt.subplots()
             self.fig.suptitle(f'{self.plug.name} Power Live [{datetime.utcnow()}] (UTC)')
-            x1_label = 'Seconds since capture'
-            self.ax1.set_xlabel(x1_label)
-            y1_label = 'Power (W)'
-            self.ax1.set_ylabel(y1_label)
+            self.ax1.set_xlabel('Time (s)')
+            self.ax1.set_ylabel('Power (W)')
             self.ax1.grid()
-            x2_label = f'{x1_label}, Buffer Length: {self.buffer_length}s'
-            self.ax2.set_xlabel(x2_label)
-            self.ax2.set_ylabel(y1_label)
-            self.ax2.grid()
-            line_style = 'g-'
-            self.ln1, = self.ax1.plot([], [], line_style)
-            self.ln2, = self.ax2.plot([], [], line_style)
-            self.ani = FuncAnimation(self.fig, self.update, interval=1000)
+            self.ln1, = self.ax1.plot([], [], 'g-')
+
+            for _ in range(n_threads):
+                threading.Thread(target=self.update, args=(start,), daemon=True).start()
+            self.ani = FuncAnimation(self.fig, start.put, interval=1000)
 
             plt.show()
         else:
+            for _ in range(n_threads):
+                threading.Thread(target=self.worker_no_graph, args=(start,), daemon=True).start()
             while True:
-                data = self.get_data()
-                self.send_to_sql(data)
+                start.put(True)
                 time.sleep(1)
 
     def get_data(self):
         data = {'timestamp': datetime.utcnow(), 'power': self.plug.get_load(), 'is_valid': 1}
+        self.captures += 1
         if self.verbose:
-            print(data)
+            print(f'[#{self.captures}]{data}')
 
         return data
 
-    def update(self, frame):
-        data = self.get_data()
-        self.update_full_graph(data)
-        self.update_buffer_graph(data)
-        self.send_to_sql(data)
-
-        return self.ln1, self.ln2
+    def update(self, start):
+        while True:
+            start.get()
+            self.check_captures_limit()
+            data = self.get_data()
+            self.update_full_graph(data)
+            self.send_to_sql(data)
 
     def update_full_graph(self, data):
         self.x1.append(self.x1[-1] + 1)
         self.y1.append(data['power'])
-        self.update_set_data(self.ax1, self.ln1, self.x1, self.y1)
-
-    def update_buffer_graph(self, data):
-        # TODO: Need to refactor this
-        if len(self.x2) < self.buffer_length:
-            self.x2.append(self.x2[-1] + 1)
-            self.y2.append(data['power'])
-        else:
-            self.x2.pop(0)
-            self.x2.append(self.x2[-1] + 1)
-            self.y2.pop(0)
-            self.y2.append(data['power'])
-        self.update_set_data(self.ax2, self.ln2, self.x2, self.y2)
-
-    @staticmethod
-    def update_set_data(ax, ln, x, y):
-        ln.set_data(x, y)
-        ax.relim()
-        ax.autoscale_view()
+        self.ln1.set_data(self.x1, self.y1)
+        self.ax1.relim()
+        self.ax1.autoscale_view()
 
     def send_to_sql(self, data):
-        # TODO: Add option to not update in real time, but in bulk and with threads
-        self.cur.execute('INSERT INTO plug_load VALUES (?, ?, ?)',
-                         (data['timestamp'], data['power'], data['is_valid']))
-        self.conn.commit()
+        with self._lock:
+            self.cur.execute(self._sql_query, (data['timestamp'], data['power'], data['is_valid']))
+            self.conn.commit()
+
+    def worker_no_graph(self, start):
+        while True:
+            start.get()
+            self.check_captures_limit()
+            data = self.get_data()
+            self.send_to_sql(data)
+
+    def check_captures_limit(self):
+        if self.captures_limit is not None and self.captures >= self.captures_limit:
+            print('Captures limit reached')
+            os._exit(1)
 
     def __del__(self):
         self.conn.close()
 
 
 if __name__ == '__main__':
-    def buffer_length_type(value):
-        ivalue = int(value)
-        if ivalue < 2:
-            raise argparse.ArgumentTypeError(f'{value} is an invalid positive int value, must be >= 2')
-
-        return ivalue
-
-
     def ip_type(value):
         if not re.match(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', value):
             raise argparse.ArgumentTypeError(f'{value} is an invalid IP address')
@@ -203,16 +190,14 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser('Plug Power Live')
     parser.add_argument('--ip', type=ip_type, required=True, help='Plug IP. Default: PLUG_IP env var')
-    parser.add_argument('-b', '--buffer_length', type=buffer_length_type, default=30,
-                        help='Buffer length in seconds, must be a positive int value >= 2. Default: 30')
     parser.add_argument('--plug_type', choices=[1, 2], default=1, type=int,
                         help='1 for Shelly Plug S, 2 for Netio PowerCable REST 101x. Default: 1')
-    parser.add_argument('--hr', action='store_true', help='Horizontal layout. Default: vertical')
     parser.add_argument('--db', required=True, help='SQLite DB file name. Default: DB_NAME env var')
     parser.add_argument('--db_reset', action='store_true', help='Delete all rows from DB table')
     parser.add_argument('--no_graph', action='store_true', help='Do not show graph')
-    parser.add_argument('-v', '--verbose', action='store_true',
-                        help='Verbose mode, print data to console')
+    parser.add_argument('--threads', type=int, default=3, help='Number of threads to use. Default: 3')
+    parser.add_argument('--captures_limit', type=int, help='Number of captures to make before exiting')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Verbose mode, print data to console')
     args = parser.parse_args()
 
     if args.plug_type == 2:
@@ -220,5 +205,5 @@ if __name__ == '__main__':
     else:
         plug_chosen = ShellyPlugS(args.ip)
 
-    PowerLive(plug_chosen, args.buffer_length, vertical=not args.hr, db_name=args.db, db_reset=args.db_reset,
-              no_graph=args.no_graph, verbose=args.verbose)
+    PowerLive(plug_chosen, db_name=args.db, db_reset=args.db_reset,
+              no_graph=args.no_graph, n_threads=args.threads, captures_limit=args.captures_limit, verbose=args.verbose)
